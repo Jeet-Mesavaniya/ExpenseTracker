060fbdb97ad7596e448732fd088d848aaee69f43
Jeet Mesavaniya <jemishmesavaniya555@gmail.com>
2023-05-11 00:54:23 +0530

    Add project files.

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //ctor command for cunstructor
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
