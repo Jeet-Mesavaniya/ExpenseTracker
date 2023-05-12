3b7da50a410de07fde198d8f65e4acb29265926c
Jeet Mesavaniya <jemishmesavaniya555@gmail.com>
2023-05-13 00:58:44 +0530

    added >>> grid for transaction, add/edit/dlt transaction ----- 13-05-2023

---
+++ ExpenseTracker/Migrations/20230512180312_Initial Create 2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Transactions",
+                newName: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Transactions",
+                newName: "Address");
+        }
+    }
+}
